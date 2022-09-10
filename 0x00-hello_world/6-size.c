5a334cf449618a4c29743d8e9d60b05a987bd184
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-09-10 13:41:01 -0700

    c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+/**
+ *
+ * * main – Print the string in the function
+ *
+ *
+ * Return: 0 number
+ */
+
+int main(void)
+
+{
+
+		printf("Size of a char: %bu byte(s)\n", sizeof(char));
+
+			printf("Size of an int: %bu byte(s)\n", sizeof(int));
+
+				printf("Size of a long int: %bu byte(s)\n", sizeof(long int));
+
+					printf("Size of a long long int: %bu byte(s)\n", sizeof(long long int));
+
+						printf("Size of a float: %bu byte(s)\n", sizeof(float));
+
+							return (0);
+
+}
+
+
