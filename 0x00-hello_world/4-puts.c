a23c3cae7a5e6733334c2c4264b1cfbf4488e155
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-09-10 13:27:02 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Dscription: Using the main function
+ * Description: Using the main function
  *
- * Return: 0
  */
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 }
