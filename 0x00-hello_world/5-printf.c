42b5950d9210cc45b4adf0218e92c75dad3ab337
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-09-10 12:26:16 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- * * main - Entry point
- *
- * * Return: 0 number
- *
- **/
-int main(void)
-
+ * *main - Entry point
+ * *
+ * *Return: Always 0
+ * */
+int main (void)
 {
+	printf(" with proper grammar, but the outcome is a piece of art, \n");
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	return (0);
 }
