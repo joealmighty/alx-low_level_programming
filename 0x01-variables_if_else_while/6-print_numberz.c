ec21c6f61cb1e2d5130c6543a74685a700c2139d
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-09-11 12:26:44 -0700

    ‘var’

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Describing the Main function
+*
+* Return: The End
+*/
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+		putchar(n);
+
+	putchar(10);
+
+	return (0);
+}
