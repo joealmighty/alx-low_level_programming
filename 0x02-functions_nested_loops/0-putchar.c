a9d059a4b83f5d0449d14c08328ad0f3881076ec
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-09-13 21:20:21 -0700

    new

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main: Decsribing the main function
+*
+* Return: Always end the Program
+*/
+int main(void)
+{
+	char string[8] = "_putchar";
+	int n = 0;
+
+	while (n < 8)
+	{
+		_putchar(string[n]);
+		++n;
+	}
+
+	_putchar(10);
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+new alx task
+
