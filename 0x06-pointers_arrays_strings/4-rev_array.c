c59fa836ad67be79d8e15acf72072ad0c5233465
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-09-21 21:56:26 -0700

    arr

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* reverse_array - reverses an array of integers 
+* reverse_array - reverses an array of integers
 *
 * @a: array to be reversed
 * @n: number of elements in the array
