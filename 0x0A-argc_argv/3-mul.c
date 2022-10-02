12bdc6b2d69ae265f306827a7dc08506e3f88d91
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-10-02 05:34:35 -0700

    arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -20,11 +20,13 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		return (1);
-
-		a = atoi(argv[1]);
+	}
+	else
+	{
+		a= atoi(argv[1]);
 		b = atoi(argv[2]);
 
 		printf("%d\n", a * b);
-
+	}
 		return (0);
 }
