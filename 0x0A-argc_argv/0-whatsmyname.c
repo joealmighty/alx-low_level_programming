b2d37ac96e24d352efe459802e5ba257e8320bf3
osamudiamen okojie <osasokojie5@gmail.com.com>
2022-10-02 04:12:38 -0700

    arg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point with argument
+*
+* @argc: counter of arguments passed
+* @argv: vector of strings with arguments passed
+*
+* Return: Always 0 (Success)
+*
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
